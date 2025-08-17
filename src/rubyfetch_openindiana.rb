9a3b17937f760e23d3389c338ad468415e683f84
Gabriell Simic <gabriellsimic5@gmail.com>
2025-08-17 15:25:48 +0200

    uptime fix

--- src/rubyfetch_openindiana.rb
+++ src/rubyfetch_openindiana.rb
@@ -8,7 +8,7 @@ shell = ENV["SHELL"].gsub(/^.+\//, "")
 distro = "openindiana"
 # uptime
 up = `uptime`.strip.gsub(/^.*up/, "").strip.gsub(/\s\s.*/, "").gsub(/,/, "").gsub(":", " hours ").gsub(/^0\shours/, "").gsub(" 1 hours", " 1 hour").gsub("min", "").strip.gsub(" 0", " ")+" mins"
-uptime = up.gsub("01", "1").gsub("02", "2").gsub("03", "3").gsub("04", "4").gsub("05", "5").gsub("06", "6").gsub("07", "7").gsub("08", "8").gsub("09", "9").gsub(" 1 mins", " 1 min").gsub(" 1 days", "1 day").gsub("00 mins", "0 mins")
+uptime = up.gsub("01", "1").gsub("02", "2").gsub("03", "3").gsub("04", "4").gsub("05", "5").gsub("06", "6").gsub("07", "7").gsub("08", "8").gsub("09", "9").gsub("1 mins", "1 min").gsub("1 days", "1 day").gsub("00 mins", "0 mins")
 # memory (it works so dont complain)
 mem = `free`.gsub(/^(Swap:).+/, "").gsub(/^\s.+/, "").strip.gsub("Mem:", "").strip.gsub(/^(\s*\d+\s+\d+).*/, '\1')
 total = mem.gsub(/(?<=\s)\d+/, "").strip.to_f / (1024**2)
